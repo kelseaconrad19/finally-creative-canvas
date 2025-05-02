89657414e899cad6f7f6922439e9a537f49d0e4e
kelseaconrad19 <163567548+kelseaconrad19@users.noreply.github.com>
2025-05-02 11:13:24 -0400

    feat: Update TinyMCE editor configuration to handle content changes

--- src/pages/AdminBlog.tsx
+++ src/pages/AdminBlog.tsx
@@ -181,7 +181,7 @@ const AdminBlog = () => {
                       'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
                       // Your account includes a free trial of TinyMCE premium features
                       // Try the most popular premium features until May 16, 2025:
-                      'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+                      // 'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
                     ],
                     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
                     tinycomments_mode: 'embedded',
@@ -193,6 +193,7 @@ const AdminBlog = () => {
                     ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
                   }}
                   initialValue="Welcome to TinyMCE!"
+                  onEditorChange={(newContent) => setContent(newContent)}
                 />
               </div>
               
